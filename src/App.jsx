c15836f23d46cba29b76ba1580ff3508e1017969
Marcuhz <121583589+Marcuhz@users.noreply.github.com>
2023-08-15 16:50:33 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import Navigation from './sections/navigation';
+import Main from './sections/main';
+import Footer from './sections/footer';
+
 import { Fragment } from 'react';
 
 export default function App() {
 	return (
 		<Fragment>
 			<Navigation />
+			<Main>
+				<a href="">/*Firefox logo*/</a>
+				<colgroup></colgroup>
+			</Main>
+			<Footer />
 		</Fragment>
 	);
 }
